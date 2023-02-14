2deb77b5a80f1aae9fa566200878d9291cfc9bcd
Elerim <pantner.andraz@gmail.com>
2023-02-14 10:19:09 +0100

    ich bin edited

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello bitch, World!");
+        System.out.println("Kill the yourself.");
     }
 }
