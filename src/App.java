fd3fa91b61eb1ab4dadaf25eacf1f6cfa448dea9
Elerim <pantner.andraz@gmail.com>
2023-02-14 10:21:20 +0100

    salam aleikum

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Kill the yourself.");
+        System.out.println("Unkill yourself :).");
     }
 }
