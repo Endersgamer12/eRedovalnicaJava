6878ebeb2c9b6dd1289782776eebb5182a1f0d04
Elerim <pantner.andraz@gmail.com>
2023-02-21 08:24:42 +0100

    minecraft

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Unkill the yourself :).");
+        System.out.println("Majkreft 😀.");
     }
 }
